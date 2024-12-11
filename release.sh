06e8bad71691ad8f1d2ecd9b63526e82ba868d11
Sunghyun Kim <hello@sunghyun.me>
2024-12-11 19:30:24 +0900

    Release version 0.0.1

--- release.sh
+++ release.sh
@@ -8,15 +8,17 @@ echo "Current version: $current_version"
 read -p "Enter new version (current is $current_version): " new_version
 
 # package.json 버전 업데이트
-npm version $new_version --no-git-tag-version
+npm version $new_version --no-git-tag-version --force
 
-# Git 커밋 및 태그 생성
-git add package.json
-git commit -m "Bump version to $new_version"
-git tag "v$new_version"
+# 모든 변경사항 커밋
+git add .
+git commit -m "Release version $new_version" --allow-empty
 
-# GitHub에 푸시
-git push && git push --tags
+# 태그 생성 및 푸시
+git tag -f "v$new_version"
+git push -f origin master
+git push -f origin "v$new_version"
 
 echo "Release process initiated for version $new_version"
 echo "GitHub Actions will automatically build and create a release"
+echo "Check the progress at: https://github.com/Kevin-Studio-Dev/SpearfishForest4-HeliosLauncher/actions"
